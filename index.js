59e3725264e7a798e87c77c49f9ca926ac63b792
ammar ahmed <boykamel8989@gmail.com>
2026-04-13 06:00:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ app.get("/", (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000);
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
